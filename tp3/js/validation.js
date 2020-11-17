e0080cdcda56d0c74857145d114eeca015bed8d5
LydiaBOUTERFA <72654876+LydiaBOUTERFA@users.noreply.github.com>
2020-11-17 16:18:58 +0100

    Delete validation.js

--- tp3/js/validation.js
+++
@@ -1,6 +0,0 @@
-$("#id_valider").click(function (event) {
-    event.preventDefault();
-    if ($("id_nom").val() != " " && $("id_prenom").val() != " " && $("id_date").val() != " " && $("id_adresse").val() != " " && $("id_mail").val() != " ") {
-        $('myModal').modal({ show: true })
-    }
-});
